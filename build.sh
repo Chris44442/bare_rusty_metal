2b735e16317476b5edc2777b102ea1df13b96cec
christoph bothe <christoph_bothe@gmx.de>
2023-10-07 22:42:18 +0200

    minor stuff

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 cargo build
+# cargo build --release
 
 arm-none-eabi-objcopy -O binary target/armv7a-none-eabi/debug/rusty_bare_metal ./bin
+# arm-none-eabi-objcopy -O binary target/armv7a-none-eabi/release/rusty_bare_metal ./bin
