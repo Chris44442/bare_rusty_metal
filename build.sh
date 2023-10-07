e8923742d7df21034a0f0553ff9f140475226397
christoph bothe <christoph_bothe@gmx.de>
2023-10-07 21:44:19 +0200

    minor changes

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cargo rustc -- -C link-arg=--script=./linker.ld -C target-cpu=cortex-a9 -C target-feature=+neon,+v7,+vfp3
+cargo rustc -- -C link-arg=--script=./linker.ld
 
 arm-none-eabi-objcopy -O binary target/armv7a-none-eabi/debug/rusty_bare_metal ./ke
